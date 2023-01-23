0230f527b719b15a6503014e86cfce56aee90b7e
Matilda Mared <matildamared@live.se>
2023-01-23 09:36:09 +0100

    add review entity class

---
+++ src/main/java/com/matildamared/diningreview/review/Review.java
@@ -0,0 +1,27 @@
+package com.matildamared.diningreview.review;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Review {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String submittedBy;
+    private Long restaurantId;
+    private String review;
+    
+    private Integer peanutScore;
+    private Integer dairyScore;
+    private Integer eggScore;
+}
