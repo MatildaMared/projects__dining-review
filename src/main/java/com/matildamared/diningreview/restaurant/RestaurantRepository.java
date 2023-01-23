fc452fcaf779948ec7bf1f4928e0f799128a42a0
Matilda Mared <matildamared@live.se>
2023-01-23 10:13:43 +0100

    add restaurant repository

---
+++ src/main/java/com/matildamared/diningreview/restaurant/RestaurantRepository.java
@@ -0,0 +1,6 @@
+package com.matildamared.diningreview.restaurant;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
+}
