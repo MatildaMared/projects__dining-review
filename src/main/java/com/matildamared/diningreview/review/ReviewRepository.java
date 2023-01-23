9488a5d38c45aff68c2e38b9e1c3618c925159b1
Matilda Mared <matildamared@live.se>
2023-01-23 10:08:29 +0100

    add review repository

---
+++ src/main/java/com/matildamared/diningreview/review/ReviewRepository.java
@@ -0,0 +1,6 @@
+package com.matildamared.diningreview.review;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReviewRepository extends CrudRepository<Review, Long> {
+}
