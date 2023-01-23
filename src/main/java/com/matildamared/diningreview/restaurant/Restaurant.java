6f4292f39752e63c6713748282498d789d4b26c0
Matilda Mared <matildamared@live.se>
2023-01-23 09:31:49 +0100

    add restaurant entity class

---
+++ src/main/java/com/matildamared/diningreview/restaurant/Restaurant.java
@@ -0,0 +1,29 @@
+package com.matildamared.diningreview.restaurant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Restaurant {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String address;
+    private String city;
+    private String postalCode;
+    private String phoneNumber;
+    private String website;
+    private String overallScore;
+    private String peanutScore;
+    private String dairyScore;
+    private String eggScore;
+}
