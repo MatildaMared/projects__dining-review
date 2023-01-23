f0701d108a1c468f18247fe8750dc187319a3105
Matilda Mared <matildamared@live.se>
2023-01-23 11:26:38 +0100

    add methods in review controller

---
+++ src/main/java/com/matildamared/diningreview/review/ReviewController.java
@@ -0,0 +1,66 @@
+package com.matildamared.diningreview.review;
+
+import com.matildamared.diningreview.restaurant.Restaurant;
+import com.matildamared.diningreview.restaurant.RestaurantRepository;
+import com.matildamared.diningreview.user.User;
+import com.matildamared.diningreview.user.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+    private final RestaurantRepository restaurantRepository;
+
+    public ReviewController(ReviewRepository reviewRepository, UserRepository userRepository, RestaurantRepository restaurantRepository) {
+        this.reviewRepository = reviewRepository;
+        this.userRepository = userRepository;
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void addUserReview(@RequestBody Review review) {
+        validateReview(review);
+
+        Optional<Restaurant> optionalRestaurant = restaurantRepository.findById(review.getRestaurantId());
+        if (optionalRestaurant.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        review.setStatus(ReviewStatus.PENDING);
+        this.reviewRepository.save(review);
+    }
+
+    private void validateReview(Review review) {
+        if (ObjectUtils.isEmpty(review.getSubmittedBy())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        if (ObjectUtils.isEmpty(review.getRestaurantId())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        if (ObjectUtils.isEmpty(review.getPeanutScore()) &&
+                ObjectUtils.isEmpty(review.getDairyScore()) &&
+                ObjectUtils.isEmpty(review.getEggScore())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        Optional<User> optionalUser = userRepository.findUserByUsername(review.getSubmittedBy());
+        if (optionalUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        Optional<Restaurant> optionalRestaurant = restaurantRepository.findById(review.getRestaurantId());
+        if (optionalRestaurant.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+}
