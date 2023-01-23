00c48f009fedd627145a00e2b1bd2db95f1a6954
Matilda Mared <matildamared@live.se>
2023-01-23 10:22:03 +0100

    update restaurant repository

--- src/main/java/com/matildamared/diningreview/restaurant/RestaurantRepository.java
+++ src/main/java/com/matildamared/diningreview/restaurant/RestaurantRepository.java
@@ -2,5 +2,15 @@ package com.matildamared.diningreview.restaurant;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
+    Optional<Restaurant> findRestaurantsByNameAndPostalCode(String name, String postalCode);
+
+    List<Restaurant> findRestaurantsByPostalCodeAndPeanutScoreNotNullOrderByPeanutScore(String zipcode);
+
+    List<Restaurant> findRestaurantsByPostalCodeAndDairyScoreNotNullOrderByDairyScore(String zipcode);
+
+    List<Restaurant> findRestaurantsByPostalCodeAndEggScoreNotNullOrderByEggScore(String zipcode);
 }
